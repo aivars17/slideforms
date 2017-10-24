7bd15861f2d80f6d5c802bbd16b47cfe0c10479b
aivars17 <30680298+aivars17@users.noreply.github.com>
2017-10-24 08:38:36 +0300

    jap

--- index.php
+++ index.php
@@ -1,106 +1,62 @@
-<?php
-date_default_timezone_set('Europe/Vilnius');
-define('SERVER', "localhost");
-define('USER', "root");
-define('PASS', "");
-define('DATABASE', "students");
-try {
-
-	$conn = new PDO("mysql:host=" . SERVER .";dbname=" . DATABASE . ";charset=utf8", USER, PASS);
-    // set the PDO error mode to exception
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$q = $conn->prepare("SELECT * FROM users"); 
-	$q->execute();
-
-	$users = $q->fetchAll(PDO::FETCH_ASSOC);
-	$conn = null;
-} catch(PDOException $e) {
-	echo "Connection failed: " . $e->getMessage();
-}
-?>
-
-
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
 	<title>Formos</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<!-- Required meta tags -->
 	<meta charset="utf-8">
-	<script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
-
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	  <style>
+	  	
+	  	h2 {
+	  		padding: 15px;
+	  	}
+	  </style>
 </head>
 <body>
 	<div class="container">
+		
+	</div>
+	<div class="container">
 		<div class="row">
-			<div class="col">
-				<div class="row">
-					<div class="col">
-						<h1>Users</h1>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col">
-						<h2>List</h2>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col">
-						<table class="table">
-							<thead>
-								<tr>
-									
-									<th>First Name</th>
-									<th>Last Name</th>
-									<th>Email</th>
-									<th>Phone number</th>
-								</tr>
-							</thead>
-							<tbody id="user_table_body">
-								<?php
-								foreach ($users as $user) {
-									echo "<tr>
-
-									<td>".$user['name']."</td>
-									<td>".$user['surname']."</td>
-									<td>".$user['email']."</td>
-									<td>".$user['phone']."</td>
-									</tr>";
-
-								}
-
-								?>
-							</tbody>
-						</table>
-					</div>
-				</div>
+			<div class="col-md-8">
+				<h2>List</h2>
+				<input class="form-control" type="number" id="filter" name="">
+				<table class="table table-sm">
+					<thead>
+						<th>#</th>
+						<th>Name</th>
+						<th>Surname</th>
+						<th>Email</th>
+						<th>Phone</th>
+					</thead>
+					<tbody id="user_table_body">
+					</tbody>
+				</table>
 			</div>
-			<div class="col">
+			<div class="col-md-4">
 				<h2>Register</h2>
 				<div id="alert"></div>
-				<form method="POST" action="submit.php">
-					<div class="input-group">
-						<input class="form-control" type="text" name="name" placeholder="Name" id="form_name">
-					</div><br/>
-					<div class="input-group">
-						<input class="form-control" type="text" name="surname"	placeholder="Surname" id="form_surname">
-					</div><br/>
-					<div class="input-group">
-						<input class="form-control" type="text" name="email" placeholder="Email" id="form_email">
-					</div><br/>
-					<div class="input-group">
-						<input class="form-control" type="text" name="phone" placeholder="Phone" id="form_phone">
-					</div><br/>
-					<div class="input-group">
-						<input class="btn btn-dark" type="submit" name="Irasyti" value="submit">
-						<input onclick="add()" class="btn btn-success" type="button" value="Add">
-						<input id="ajax_post" class="btn btn-warning" type="button" value="Add">
-					</div>
-					
-				</form>	
+				<div class="input-group">
+					<input id="form_name" class="form-control" type="text" name="name" placeholder="Name">
+				</div><br/>
+				<div class="input-group">
+					<input id="form_surname" class="form-control" type="text" name="surname" placeholder="Surname">
+				</div><br/>
+				<div class="input-group">
+					<input id="form_email" class="form-control" type="email" name="email" placeholder="Email">
+				</div><br/>
+				<div class="input-group">
+					<input  id="form_phone" class="form-control" type="text" name="phone" placeholder="Phone Number">
+				</div><br/>
+				<div class="input-group">
+					<input id="ajax_post" class="btn btn-danger" type="button" value="Add user">
+				</div>
 			</div>
 		</div>
 	</div>
-	<script src="js/script.js"></script>
+<script src="js/script.js"></script>
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,30 +1,54 @@
-function add() {
-			name = document.getElementById('form_name').value;
-			surname = document.getElementById('form_surname').value;
-			email = document.getElementById('form_email').value;
-			phone = document.getElementById('form_phone').value;
-			
-			user_table_body = document.getElementById("user_table_body");
-			user_table_body.innerHTML += "<tr><td>" + name + "</td><td>" + surname + "</td><td>" + email + "</td><td>" + phone + "</td></tr>";
-		}
+$("#ajax_post").click(function(){
 
-		$("#ajax_post").click(function(){
-			$.post("submit.php",
-			{
-				name: $("#form_name").val(),
-				surname: $("#form_surname").val(),
-				email: $("#form_email").val(),
-				phone: $("#form_phone").val()
-			},
-			function(data, status){
-				$("#alert").html(data);
-				$.getJSON("show.php", function(result){
-					$("#user_table_body").html('');
-				$.each(result, function(i,field){
-					$("#user_table_body").append("<tr><td>" + field.name + "</td><td>" + field.surname + "</td><td>" + field.email + "</td><td>" + field.phone + "</td></tr><br/>");
-				});
+	$.post("users.php",
+	{
+		name: $("#form_name").val(),
+		surname: $("#form_surname").val(),
+		email: $("#form_email").val(),
+		phone: $("#form_phone").val()
+	},
+	function(data, status){
+
+		$("#alert").html("<div class='alert alert-"+data.message.type+"'>" + data.message.body + "</div>");
+	   	$.getJSON("users.php", function(result){
+
+			$("#user_table_body").html('');
+			$.each(result['users'], function(i, field){
+				$("#user_table_body").append("<tr><td>" + field.name + "</td><td>" + field.surname + "</td><td>" + field.email + "</td><td>" + field.phone + "</td></tr>");
 			});
-
-			});
-
 		});
+	});
+
+});
+
+$.getJSON("users.php", function(result){
+	$("#user_table_body").html('');
+	$.each(result['users'], function(i, field){
+		$("#user_table_body").append("<tr><td>" + field.id + "</td><td>" + field.name + "</td><td>" + field.surname + "</td><td>" + field.email + "</td><td>" + field.phone + "</td></tr>");
+	});
+});
+
+
+
+$("#filter").keyup(function(){
+	console.log($("#filter").val());
+	$.getJSON("users.php",
+	{
+		filter: $("#filter").val(),
+	},
+	 function(result){
+
+			$("#user_table_body").html('');
+			$.each(result['users'], function(i, field){
+				$("#user_table_body").append("<tr><td>" + field.id + "</td><td>" + field.name + "</td><td>" + field.surname + "</td><td>" + field.email + "</td><td>" + field.phone + "</td></tr>");
+	});
+
+});
+});
+
+$.getJSON("users.php", function(result){
+	$("#user_table_body").html('');
+	$.each(result['users'], function(i, field){
+		$("#user_table_body").append("<tr><td>" + field.id + "</td><td>" + field.name + "</td><td>" + field.surname + "</td><td>" + field.email + "</td><td>" + field.phone + "</td></tr>");
+	});
+});
